3f3abc0c9a112718bc523873e90a29e1cf7cf743
Zaire McAllister <ZaireM21@gmail.com>
2025-07-11 20:01:51 -0400

    Initial Commit

---
+++ tmp-weather-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tmp-weather-app
+//
+//  Created by Zaire McAllister on 7/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
